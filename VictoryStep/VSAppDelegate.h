3a75ddc06466a50bc2d06730959df15fdc585dcd
rcadmin <rcadmin@XMNA006.local>
2014-05-06 17:43:47 +0800

    Initial Commit

---
+++ VictoryStep/VSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VSAppDelegate.h
+//  VictoryStep
+//
+//  Created by rcadmin on 5/6/14.
+//  Copyright (c) 2014 StevenZhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VictoryStep/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VictoryStep
+//
+//  Created by rcadmin on 5/6/14.
+//  Copyright (c) 2014 StevenZhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VSAppDelegate class]));
+    }
+}
